38a7102017e5e149967f9050fd704f498b3e3af4
Space Cowboy <spacecowboy790@gmail.com>
2021-03-22 19:58:13 +0700

    Lesson 2 HM 22/03/21

--- Lesson_2/GuessNumberTest.java
+++ Lesson_2/GuessNumberTest.java
@@ -2,15 +2,19 @@ import java.util.Scanner;
 
 public class GuessNumberTest {
     public static void main(String[] args) {
+        GuessNumber guessNumber = enterPlayersNames();
+        do {
+            guessNumber.playGame();
+        } while (isExist());
+    }
+
+    public static GuessNumber enterPlayersNames() {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Введите имя первого игрока: ");
         Player player1 = new Player(scanner.nextLine());
         System.out.println("Введите имя второго игрока: ");
         Player player2 = new Player(scanner.nextLine());
-        GuessNumber guessNumber = new GuessNumber(player1, player2);
-        do {
-            guessNumber.playGame();
-        } while (isExist());
+        return new GuessNumber(player1, player2);
     }
 
     public static boolean isExist() {
