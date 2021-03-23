6d4dffbbadf4ac6efa29d6d9890abc8909943f0a
Space Cowboy <spacecowboy790@gmail.com>
2021-03-24 01:03:48 +0700

    Lesson 2 HM 24/03/21

--- Lesson_2/GuessNumber.java
+++ Lesson_2/GuessNumber.java
@@ -14,19 +14,16 @@ public class GuessNumber {
     public void start() {
         Random random = new Random();
         int targetNumber = random.nextInt(100) + 1;
-        //System.out.println("Число, которое загадал компьютер: " + targetNumber);
+        System.out.println("Число, которое загадал компьютер: " + targetNumber);
         do {
             enterNumber(player1);
-            if (checkForFinish(player1.getNumber(), targetNumber)) {
-                System.out.println(player1.getName() + " победил");
+            if (checkForFinish(player1, player1.getNumber(), targetNumber)) {
                 break;
-            } else continueGame(player1, player1.getNumber(), targetNumber);
-
+            }
             enterNumber(player2);
-            if (checkForFinish(player2.getNumber(), targetNumber)) {
-                System.out.println(player2.getName() + " победил");
+            if (checkForFinish(player2, player2.getNumber(), targetNumber)) {
                 break;
-            } else continueGame(player2, player2.getNumber(), targetNumber);
+            }
         } while (true);
     }
 
@@ -36,13 +33,16 @@ public class GuessNumber {
         player.setNumber(scanner.nextInt());
     }
 
-    private boolean checkForFinish(int playerNumber, int targetNumber) {
-        return playerNumber == targetNumber;
-    }
-
-    private void continueGame(Player player, int playerNumber, int targetNumber) {
+    private boolean checkForFinish(Player player, int playerNumber, int targetNumber) {
         if (playerNumber > targetNumber) {
             System.out.println("Число меньше того, что назвал " + player.getName());
-        } else System.out.println("Число больше того, что назвал " + player.getName());
+            return false;
+        } else if (playerNumber < targetNumber) {
+            System.out.println("Число больше того, что назвал " + player.getName());
+            return false;
+        } else {
+            System.out.println(player.getName() + " победил");
+            return true;
+        }
     }
 }
