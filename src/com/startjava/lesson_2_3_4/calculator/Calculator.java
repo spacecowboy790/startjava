320a8c2f3fd9578f8c909b7257cedf466da486c7
Space Cowboy <spacecowboy790@gmail.com>
2021-03-26 23:07:15 +0700

    Lesson 4 HM 26/03/21

--- src/com/startjava/lesson_2_3_4/calculator/Calculator.java
+++ src/com/startjava/lesson_2_3_4/calculator/Calculator.java
@@ -2,40 +2,30 @@ package com.startjava.lesson_2_3_4.calculator;
 
 public class Calculator {
 
-    private int num1;
-    private int num2;
-    private char operation;
+    private String[] mathExpression;
 
-    public void setNum1(int num1) {
-        this.num1 = num1;
-    }
-
-    public void setOperation(char operation) {
-        this.operation = operation;
-    }
-
-    public void setNum2(int num2) {
-        this.num2 = num2;
+    public void setMathExpression(String[] mathExpression) {
+        this.mathExpression = mathExpression;
     }
 
     public int calculate() {
+        int num1 = Integer.parseInt(mathExpression[0]);
+        String operation = mathExpression[1];
+        int num2 = Integer.parseInt(mathExpression[2]);
+
         switch (operation) {
-            case '+':
+            case "+":
                 return num1 + num2;
-            case '-':
+            case "-":
                 return num1 - num2;
-            case '*':
-                return num1 * num2;
-            case '/':
-                return num1 / num2;
-            case '^':
-                int res = 1;
-                for (int i = 1; i <= num2; i++) {
-                    res *= num1;
-                }
-                return res;
-            case '%':
-                return num1 % num2;
+            case "*":
+                return Math.multiplyExact(num1, num2);
+            case "/":
+                return Math.floorDiv(num1, num2);
+            case "^":
+                return (int) Math.pow(num1, num2);
+            case "%":
+                return Math.floorMod(num1, num2);
             default:
                 System.out.println("Неверная операция");
         }
--- src/com/startjava/lesson_2_3_4/calculator/CalculatorTest.java
+++ src/com/startjava/lesson_2_3_4/calculator/CalculatorTest.java
@@ -13,12 +13,8 @@ public class CalculatorTest {
 
     public static void enterMathExpression(Calculator calculator) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Введите первое число:");
-        calculator.setNum1(scanner.nextInt());
-        System.out.println("Введите знак математической операции:");
-        calculator.setOperation(scanner.next().charAt(0));
-        System.out.println("Введите второе число:");
-        calculator.setNum2(scanner.nextInt());
+        System.out.println("Введите математическое выражение:");
+        calculator.setMathExpression(scanner.nextLine().split(" "));
     }
 
     public static boolean isExist() {
