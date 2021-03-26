c47b86537307c86ce4f243e9cdca5c8c6bad6a4a
Space Cowboy <spacecowboy790@gmail.com>
2021-03-27 01:29:03 +0700

    Lesson 4 HM 26/03/21

--- src/com/startjava/lesson_2_3_4/game/GuessNumber.java
+++ src/com/startjava/lesson_2_3_4/game/GuessNumber.java
@@ -1,5 +1,6 @@
 package com.startjava.lesson_2_3_4.game;
 
+import java.util.Arrays;
 import java.util.Random;
 import java.util.Scanner;
 
@@ -16,23 +17,35 @@ public class GuessNumber {
     public void start() {
         Random random = new Random();
         int targetNumber = random.nextInt(100) + 1;
+
+        // индекс для наихудщего исхода: если не будет введено загаданное компьютером число
+        int indexForWin = 10;
         //System.out.println("Число, которое загадал компьютер: " + targetNumber);
-        do {
-            enterNumber(player1);
+
+        System.out.println("У вас 10 попыток");
+        for (int i = 0; i < 10; i++) {
+            player1.getEnteredNumbers()[i] = enterNumber(player1);
             if (compareNumbers(player1, targetNumber)) {
+                indexForWin = i + 1;
                 break;
             }
-            enterNumber(player2);
+            player2.getEnteredNumbers()[i] = enterNumber(player2);
             if (compareNumbers(player2, targetNumber)) {
+                indexForWin = i + 1;
                 break;
             }
-        } while (true);
+        }
+
+        printEnteredNumbers(player1, indexForWin);
+        printEnteredNumbers(player2, indexForWin);
+        printResults(player1, player2, targetNumber, indexForWin);
     }
 
-    private void enterNumber(Player player) {
+    private int enterNumber(Player player) {
         Scanner scanner = new Scanner(System.in);
         System.out.println(player.getName() + ", введите число:");
         player.setNumber(scanner.nextInt());
+        return player.getNumber();
     }
 
     private boolean compareNumbers(Player player, int targetNumber) {
@@ -43,5 +56,35 @@ public class GuessNumber {
         String result = player.getNumber() > targetNumber ? "меньше" : "больше";
         System.out.println("Число " + result + " того, что назвал " + player.getName());
         return false;
+    }
+
+    private void printEnteredNumbers(Player player, int indexForWin) {
+        int[] res = Arrays.copyOf(player.getEnteredNumbers(), indexForWin);
+        System.out.print("Игрок " + player.getName() + " ввел числа: ");
+        for (int i : res) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+    }
+
+    private void printResults(Player player1, Player player2, int targetNumber, int indexForWin) {
+        if (player1.getEnteredNumbers()[indexForWin - 1] == targetNumber) {
+            printWinMessage(player1.getName(), targetNumber, indexForWin);
+        } else if (player2.getEnteredNumbers()[indexForWin - 1] == targetNumber) {
+            printWinMessage(player2.getName(), targetNumber, indexForWin);
+        } else {
+            printFailMessage();
+        }
+        Arrays.fill(player1.getEnteredNumbers(), 0, indexForWin, 0);
+    }
+
+    private void printWinMessage(String name, int targetNumber, int indexForWin) {
+        System.out.println("Игрок " + name + " угадал число " + targetNumber + " с " +
+                indexForWin + " попытки");
+    }
+
+    private void printFailMessage() {
+        System.out.println("У " + player1.getName() + " закончились попытки");
+        System.out.println("У " + player2.getName() + " закончились попытки");
     }
 }
--- src/com/startjava/lesson_2_3_4/game/Player.java
+++ src/com/startjava/lesson_2_3_4/game/Player.java
@@ -4,9 +4,14 @@ public class Player {
 
     private String name;
     private int number;
+    private int[] enteredNumbers = new int[10];
 
     public Player(String name) {
         this.name = name;
+    }
+
+    public int[] getEnteredNumbers() {
+        return enteredNumbers;
     }
 
     public String getName() {
